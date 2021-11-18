77deb98bc20c197c1ca92fe379e7c7737c1c8199
NasonovaYana <yokeanova@gmail.com>
2021-11-18 17:12:25 +0300

    fnc_get

--- 5/functions_get.php
+++ 5/functions_get.php
@@ -12,9 +12,10 @@ echo "
 <input type='submit' value = 'Отправить'>
 </form>
 ";
-
-$name = $_GET['name'];
-echo "Привет, $name!<br><br>";
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];
+    echo "Привет, $name!<br><br>";
+}
 
 //Спросите у пользователя имя, возраст, а также попросите его ввести сообщение (его сделайте в textarea). Выведите эти данные на экран в формате, приведенном под данной задачей. Позаботьтесь о том, чтобы пользователь не мог вводить теги (просто удаляйте их) и таким образом сломать сайт.
 
@@ -26,11 +27,12 @@ echo "
 <input type='submit' value = 'Отправить'>
 </form>
 ";
-$userName = $_GET['userName'];
-$age = $_GET['userAge'];
-$message = strip_tags($_GET['message']);
-echo "Привет, $userName, $age лет.<br> Твоё сообщение: $message<br><br>";
-
+if (isset($_GET['userName']) and isset($_GET['userAge']) and isset($_GET['message'])) {
+    $userName = $_GET['userName'];
+    $age = $_GET['userAge'];
+    $message = strip_tags($_GET['message']);
+    echo "Привет, $userName, $age лет.<br> Твоё сообщение: $message<br><br>";
+}
 
 //Спросите возраст пользователя. Если форма была отправлена и введен возраст, то выведите его на экран, а форму уберите. Если же форма не была отправлена (это будет при первом заходе на страницу) - просто покажите ее.
 
@@ -49,9 +51,62 @@ if (!isset($_GET['age'])) {
 echo "
 <form action = '' method='get'>
 <input placeholder='Введите логин' name = 'login'> <br><br>
-<input placeholder='Введите пароль' name = 'password'> <br><br>
+<input placeholder='Введите пароль' type='password' name = 'password'> <br><br>
 <input type='submit' value = 'Отправить'>
 </form>
 ";
+$login = 'qwerty';
+$pass = '12345';
+
+if (isset($_GET['login']) and isset($_GET['password'])) {
+    $loginUsers = trim($_GET['login']);
+    $password = trim($_GET['password']);
+    if ($password == $pass and $loginUsers == $login) {
+        echo "Доступ разрешён!<br>";
+    } else {
+        echo "Доступ запрещён!<br>";
+    }
+}
+
 //Спросите имя пользователя с помощью формы. Результат запишите в переменную $name. Сделайте так, чтобы после отправки формы значения ее полей не пропадали.
+
+if (!isset($_GET['nameUser'])) {
+    echo "
+<form action = '' method='get'>
+<input placeholder='Введите имя'  name = 'nameUser'>
+<input type='submit' value = 'Отправить'>
+</form>
+";
+} else {
+    $nameUser = $_GET['nameUser'];
+
+    echo "
+<form action = '' method='get'>
+<input placeholder='Введите имя'  name = 'nameUser' value=$nameUser>
+<input type='submit' value = 'Отправить'>
+</form>
+";
+    echo $nameUser;
+}
+
 //Спросите у пользователя имя, а также попросите его ввести сообщение (textarea). Сделайте так, чтобы после отправки формы значения его полей не пропадали.
+if (!isset($_GET['nameUserAgain']) and !isset($_GET['userMassage'])) {
+    echo "
+<form action = '' method='get'>
+<input placeholder='Введите имя'  name = 'nameUserAgain'><br>
+<textarea name ='userMessage'></textarea><br>
+<input type='submit' value = 'Отправить'>
+</form>
+";
+} else {
+    $a = 'gy';
+    $nameUserAgain = $_GET['nameUserAgain'];
+    $userMessage = $_GET['userMessage'];
+    echo "
+<form action = '' method='get'>
+<input placeholder='Введите имя'  name = 'nameUserAgain' value=$nameUserAgain><br>
+<textarea name ='userMessage'>$userMessage</textarea><br>
+<input type='submit' value = 'Отправить'>
+</form>
+";
+}
