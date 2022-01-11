a0d74896df148238065d2454458b6fb87d1ef02a
sbeketov <sergei_beketov@senla.eu>
2022-01-11 14:03:40 +0300

    Add helpers

---
+++ helpers.php
@@ -0,0 +1,45 @@
+<?php
+
+function d($data){
+    if(is_null($data)){
+        $str = "<i>NULL</i>";
+    }elseif($data == ""){
+        $str = "<i>Empty</i>";
+    }elseif(is_array($data)){
+        if(count($data) == 0){
+            $str = "<i>Empty array.</i>";
+        }else{
+            $str = "<table style=\"border-bottom:0 solid #000; border-collapse: collapse\">";
+            foreach ($data as $key => $value) {
+                $str .= "<tr><td style=\"background-color:#008B8B; color:#FFF;border:1px solid #000; padding: 5px;\">" . $key . "</td><td style=\"border:1px solid #000;\">" . d($value) . "</td></tr>";
+            }
+            $str .= "</table>";
+        }
+//    }elseif(is_resource($data)){
+//        while($arr = mysql_fetch_array($data)){
+//            $data_array[] = $arr;
+//        }
+//        $str = d($data_array);
+    }elseif(is_object($data)){
+        $str = d(get_object_vars($data));
+    }elseif(is_bool($data)){
+        $str = "<i>" . ($data ? "True" : "False") . "</i>";
+    }else{
+        $str = $data;
+        $str = preg_replace("/\n/", "<br>\n", $str);
+    }
+    return $str;
+}
+
+function dump($data){
+    echo d($data) . "<br>\n";
+}
+
+function dd($data){
+    dump($data);
+    exit;
+}
+
+function ddt($message = ""){
+    echo "[" . date("Y/m/d H:i:s") . "]" . $message . "<br>\n";
+}
