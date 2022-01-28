da79ab11818ec9616090dea0a1497f0f8f9f6d66
NasonovaYana <74826480+NasonovaYana@users.noreply.github.com>
2022-01-28 17:52:15 +0300

    Delete 8 directory

--- 8/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$forms = include './sessions_forms.phtml';
-echo $forms['countryUser'];
-if( isset($_POST['usersCountry'])) {
-    $usersCountry = $_POST['usersCountry'];
-    $_SESSION['usersCountry'] = $usersCountry;
-}
-echo $forms['hrefTest'];
--- 8/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$forms = include './sessions_forms.phtml';
-if(isset($_SESSION['usersCountry'])){
-    echo $_SESSION['usersCountry'];
-}else{
-    echo "Страна не установлена, перейдите на index.php";
-    echo $forms['hrefIndex'];
-}
